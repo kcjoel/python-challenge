f66b0d1c505b7e588a45fd22aeb3b3ece08abf44
kcjoel <kc4jomi@gmail.com>
2023-10-10 05:57:08 -0500

    final committ

---
+++ pyBank/analysis/pyBank.py
@@ -0,0 +1,61 @@
+import os
+import csv
+
+#introducing the path to our file
+csvpath = os.path.join("Resources","budget_data.csv")
+
+#opening up the file within our path
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header= next(csvreader)
+    print(csv_header)#extracting header from data file
+    
+#setting up variables    
+    TM=0 #Total Month
+    TPL=0 #Total Profit/Loss
+    y1=0 #first item for profit/loss
+    changeNet=[] 
+    great_inc=['',0] #Used for calculating greatest P/L increase change
+    great_dec=[0,99999999999] #Used for calculating the greatest P/L decrease
+    PNet = 0
+  
+#extracts our data in rows and columns
+    for row in csvreader:
+        print(row[0],row[1])
+        TM=TM+1
+        TPL= TPL+float(row[1])
+        
+#calculating data used for average profit/loss        
+        if TM==1:
+            y1=float(row[1])
+            
+#Tracking the change in profit/loss    
+        change= float(row[1])-PNet
+        PNet=float(row[1])
+        changeNet+= [change]
+       
+        
+#calculating the greatest increase
+        if change > great_inc[1]:
+            great_inc[0] = row[0]
+            great_inc[1]=  change
+        
+#calculating the greatest decrease
+        if change < great_dec[1]:
+            great_dec[0] = row[0]
+            great_dec[1]=  change
+            
+#calculating the average change        
+    AC=(float(row[1])-y1)/(TM-1)
+
+#exporting our final results to a CSV file
+output_path= os.path.join("analysis","FA1.csv")
+with open(output_path,'w') as csvfile:
+    csvwriter= csv.writer(csvfile,delimiter=",")
+    csvwriter.writerow(['FINANCIAL ANALYSIS',])
+    csvwriter.writerow(["-----------------------"])
+    csvwriter.writerow([f"Total Months: ",{TM}])
+    csvwriter.writerow([f"Total: ",{TPL}])  
+    csvwriter.writerow([f"Average Change: ",{AC}])
+    csvwriter.writerow([f"Greatest Increase in Profits:",great_inc[0],{great_inc[1]}])
+    csvwriter.writerow([f"Greatest Decrease in Profits:",great_dec[0],{great_dec[1]}])
---
+++ pyPoll/analysis/pyPoll.py
@@ -0,0 +1,71 @@
+#importing modules
+import os
+import csv
+
+#introducing the path to our file
+csvpath = os.path.join("..","Resources","election_data.csv")
+
+#opening up the file within our path
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header= next(csvreader)
+    print(csv_header)#extracting header from data file
+    
+#setting up variables    
+    TV=0
+    CA=0
+    CAL=[]
+    new=[]
+    cand2_count = cand1_count = cand3_count = 0
+    cand1_perc= cand1_perc = cand3_perc =0
+#used new to see all candidiates who received votes
+    cand1="Diana DeGette"
+    cand2="Raymon Anthony Doane"
+    cand3="Charles Casper Stockham"
+    
+   
+    
+    
+#extracts our data in rows and columns
+    for row in csvreader:
+        TV=TV+1
+        if CA != row[2]:
+            CA=row[2]
+            CAL+=[CA]
+            new=set(CAL)
+    
+
+#candidiate calculation for amount of votes received
+    
+        if row[2]==cand1:
+            cand1_count= cand1_count+1
+        elif cand2==row[2]:
+            cand2_count= cand2_count+1
+        elif row[2]== cand3:
+            cand3_count= cand3_count+1
+            
+#candidiate calculation for the percentage of votes each candidiates received
+
+    cand1_perc= round((cand1_count/TV)*100,3)  
+    cand2_perc= round((cand2_count/TV)*100,3) 
+    cand3_perc= round((cand3_count/TV)*100,3) 
+    
+#creating a dictionary so we can extract maximum value based on candidiates
+    results={cand1:cand1_count,cand2:cand2_count,cand3:cand3_count}
+    winner=max(results,key=results.get)
+    
+#Extracting results as CSV file
+    
+    output_path= os.path.join("..","analysis","FA2.csv")
+with open(output_path,'w') as csvfile:
+    csvwriter= csv.writer(csvfile)
+    csvwriter= csv.writer(csvfile)
+    csvwriter.writerow(["Election Results"])
+    csvwriter.writerow(["-----------------------"])
+    csvwriter.writerow([f"Total Votes: ",{TV}])  
+    csvwriter.writerow(["-------------------------"]),
+    csvwriter.writerow([f"{cand3}: {cand3_perc}% ({cand3_count})"])
+    csvwriter.writerow([f"{cand1}: {cand1_perc}% ({cand1_count})"])
+    csvwriter.writerow([f"{cand2}: {cand2_perc}% ({cand2_count})"])
+    csvwriter.writerow(["-----------------------"])
+    csvwriter.writerow([f"Winner: ",{winner}])
